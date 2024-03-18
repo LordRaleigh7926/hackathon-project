f764d7d5a05f63d2cd3970a46c79418ac367deb3
Ankush Roy <97963983+LordRaleigh7926@users.noreply.github.com>
2024-03-18 23:39:18 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,14 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCGTklMpO4eY00Ri9q3pwkg8B672OgsGyg",
-    authDomain: "education-proj-hackathon.firebaseapp.com",
-    projectId: "education-proj-hackathon",
-    storageBucket: "education-proj-hackathon.appspot.com",
-    messagingSenderId: "1087488082925",
-    appId: "1:1087488082925:web:718f6aa2ad4eac19531de4"
-  };
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
